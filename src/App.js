294833f7d47ea1d53228e9ba90b7c3c556d01b05
ubaidchalil <c.ubaid@gmail.com>
2019-11-15 23:13:58 +0530

    cleaned some files

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import initStore from './store';
 import {Platform} from 'react-native';
 import RootContainer from './rootContainer';
 const {store, persistor} = initStore();
-import {Sentry, SentrySeverity, SentryLog} from 'react-native-sentry';
-const sentryDsn = Platform.select({
-  android: 'https://1d19dbd920d1454d848c6e17d358f6b5@sentry.io/1531525',
-  ios: 'https://1d19dbd920d1454d848c6e17d358f6b5@sentry.io/1531525',
-});
-Sentry.config(sentryDsn, {
-  logLevel: SentryLog.Debug,
-  deactivateStacktraceMerging: false,
-}).install();
+// import {Sentry, SentrySeverity, SentryLog} from 'react-native-sentry';
+// const sentryDsn = Platform.select({
+//   android: 'https://1d19dbd920d1454d848c6e17d358f6b5@sentry.io/1531525',
+//   ios: 'https://1d19dbd920d1454d848c6e17d358f6b5@sentry.io/1531525',
+// });
+// Sentry.config(sentryDsn, {
+//   logLevel: SentryLog.Debug,
+//   deactivateStacktraceMerging: false,
+// }).install();
 
 import OneSignal from 'react-native-onesignal';
 OneSignal.init('c6ea3452-bace-4056-9d63-c2c4f4f58643', {
